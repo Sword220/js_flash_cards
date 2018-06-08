f763c0f1041c1077c9f23e13f4bc871ce0d34f4d
Pete Sword <pete.sword@yahoo.com>
2018-06-07 23:45:39 -0600

    initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready( function() {
+  $(".card1").on('click', function() {
+    $(".card1").flip();
+  })
+  $(".card2").on('click', function() {
+    $(".card2").flip();
+  })
+  $(".card3").on('click', function() {
+    $(".card3").flip();
+  })
+})
+
+
+
